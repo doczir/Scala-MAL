0a1bc34ca517de6ecef1e6afda21eec725651dac
Robert Doczi <doczi.r@gmail.com>
2017-01-28 16:41:47 +0100

    added list and comparison operations to prelude

--- src/main/scala/hu/lexan/mal/eval/Prelude.scala
+++ src/main/scala/hu/lexan/mal/eval/Prelude.scala
@@ -49,6 +49,47 @@ object Prelude {
     Right(MList(args))
   }
 
+  private def list_test(args: List[MalExpr]): Either[MalError, MalExpr] = args match {
+    case (_: MList) :: _ => Right(MTrue)
+    case _ => Right(MFalse)
+  }
+
+  private def empty_test(args: List[MalExpr]): Either[MalError, MalExpr] = args match {
+    case MList(elements) :: _ if elements.isEmpty => Right(MTrue)
+    case _ => Right(MFalse)
+  }
+
+  private def count(args: List[MalExpr]): Either[MalError, MalExpr] = args match {
+    case MList(elements) :: _ => Right(elements.length.mi)
+    case _ => Right(0.mi)
+  }
+
+  private def equal_test(args: List[MalExpr]): Either[MalError, MalExpr] = args match {
+    case a1 :: a2 :: _ => Right((a1 == a2).mb)
+    case _ => Right(MFalse)
+  }
+
+  private def lt(args: List[MalExpr]): Either[MalError, MalExpr] = args match {
+    case MInteger(a1) :: MInteger(a2) :: Nil => Right((a1 < a2).mb)
+    case _ => Left(MalEvaluationError("Illegal arguments to <", MList("<".msym :: args)))
+  }
+
+  private def gt(args: List[MalExpr]): Either[MalError, MalExpr] = args match {
+    case MInteger(a1) :: MInteger(a2) :: Nil => Right((a1 > a2).mb)
+    case _ => Left(MalEvaluationError("Illegal arguments to >", MList(">".msym :: args)))
+  }
+
+  private def lte(args: List[MalExpr]): Either[MalError, MalExpr] = args match {
+    case MInteger(a1) :: MInteger(a2) :: Nil => Right((a1 <= a2).mb)
+    case _ => Left(MalEvaluationError("Illegal arguments to <=", MList("<=".msym :: args)))
+  }
+
+  private def gte(args: List[MalExpr]): Either[MalError, MalExpr] = args match {
+    case MInteger(a1) :: MInteger(a2) :: Nil => Right((a1 >= a2).mb)
+    case _ => Left(MalEvaluationError("Illegal arguments to >=", MList(">=".msym :: args)))
+  }
+
+
   val env: Environment = {
     val env = new Environment()
 
@@ -56,11 +97,18 @@ object Prelude {
     env.define("-".msym, MFunction(sub))
     env.define("*".msym, MFunction(mul))
     env.define("/".msym, MFunction(div))
-    env.define("list".msym, MFunction(list))
     env.define("pr-str".msym, MFunction(pr_str))
     env.define("str".msym, MFunction(str))
     env.define("prn".msym, MFunction(prn))
     env.define("println".msym, MFunction(prnln))
+    env.define("list".msym, MFunction(list))
+    env.define("list?".msym, MFunction(list_test))
+    env.define("empty?".msym, MFunction(empty_test))
+    env.define("count".msym, MFunction(count))
+    env.define("<".msym, MFunction(lt))
+    env.define(">".msym, MFunction(gt))
+    env.define("<=".msym, MFunction(lte))
+    env.define(">=".msym, MFunction(gte))
     env
   }
 
