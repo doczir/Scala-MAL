46f7ccb7d24855ca005b15b1a1b647fbf11416e1
Robert Doczi <doczi.r@gmail.com>
2017-01-28 22:07:27 +0100

    extracted ast printer from types

--- src/main/scala/hu/lexan/mal/ast/MalTypes.scala
+++ src/main/scala/hu/lexan/mal/ast/MalTypes.scala
@@ -58,38 +58,4 @@ object MalAstExtensions {
   }
 }
 
-// ===================================
-//                UTILS
-// ===================================
-object AstPrinter {
-  private def unescape(c: Char): String = c match {
-    case '\n' => "\\n"
-    case '\\' => "\\\\"
-    case '"' => "\\\""
-    case anychar => anychar.toString
-  }
 
-  def print(expr: MalExpr, readable: Boolean = true): String = expr match {
-    case MSymbol(name) => name
-    case MKeyword(kw) => s":$kw"
-    case MString(str) => if (readable) s""""${str.flatMap(unescape)}"""" else str
-    case MInteger(value) => value.toString
-    case MList(nodes) => s"(${
-      nodes.map {
-        print(_, readable)
-      }.mkString(" ")
-    })"
-    case MVector(nodes) => s"[${
-      nodes.map {
-        print(_, readable)
-      }.mkString(" ")
-    }]"
-    case MMap(elems) => s"{${
-      elems.toList.map { case (key, value) => s"${print(key)} ${print(value)}" }.mkString(" ")
-    }}"
-    case MNil => "nil"
-    case MTrue => "true"
-    case MFalse => "false"
-    case _: MFunction => "#<function>"
-  }
-}
