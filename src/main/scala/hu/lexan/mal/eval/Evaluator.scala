282efe423d7a7894b7351afe9088cc3e8791caf6
Robert Doczi <doczi.r@gmail.com>
2017-01-28 16:58:58 +0100

    switched liftError to sequence and traverse

--- src/main/scala/hu/lexan/mal/eval/Evaluator.scala
+++ src/main/scala/hu/lexan/mal/eval/Evaluator.scala
@@ -1,16 +1,58 @@
 package hu.lexan.mal.eval
 
+import cats.instances.all._
+import cats.syntax.traverse._
 import hu.lexan.mal.ast._
 import hu.lexan.mal.error.{MalError, MalEvaluationError}
 
 object Evaluator {
+
   def evaluate(ast: MalExpr): Either[MalError, MalExpr] = {
     evaluate(ast, Prelude.env)
   }
 
-  def evaluate(ast: MalExpr, env: Environment): Either[MalError, MalExpr] = ast match {
-    case list: MList => applyAst(list, env)
-    case _ => evaluate_ast(ast, env)
+  private def letEnv(defs: List[MalExpr], outer: Environment): Either[MalError, Environment] = {
+    val newEnv = new Environment(Some(outer))
+
+    val definitions = defs.grouped(2).map {
+      case (key: MSymbol) :: value :: Nil => Right((key, value))
+      case elements => Left(MalEvaluationError(s"Key '${elements.head}' is not a symbol", MList(defs)))
+    }.toStream.sequenceU
+    definitions.flatMap { dfs =>
+      dfs.foldLeft(Right(newEnv): Either[MalError, Environment]) { case (eitherEnv, (key, value)) =>
+        for {
+          env <- eitherEnv
+          evaluated <- evaluate(value, env)
+        } yield {
+          newEnv.define(key, evaluated)
+          newEnv
+        }
+      }
+    }
+  }
+
+  private def handleLet(ast: MList, env: Environment, rest: List[MalExpr]) = {
+    rest match {
+      case (MList(definitions) :: expr :: Nil) =>
+        letEnv(definitions, env).flatMap { newEnv =>
+          evaluate(expr, newEnv)
+        }
+      case (MVector(definitions) :: expr :: Nil) =>
+        letEnv(definitions, env).flatMap { newEnv =>
+          evaluate(expr, newEnv)
+        }
+      case _ => Left(MalEvaluationError("Illegal 'let*' expression", ast))
+    }
+  }
+
+  def handleDo(ast: MList, env: Environment, rest: List[MalExpr]): Either[MalError, MalExpr] = {
+    val it = rest.iterator
+    var evaluated: Either[MalError, MalExpr] = null
+    while (it.hasNext) {
+      evaluated = evaluate(it.next(), env)
+      if (evaluated.isLeft) return evaluated
+    }
+    evaluated
   }
 
   private def applyAst(ast: MList, env: Environment): Either[MalError, MalExpr] = ast match {
@@ -18,6 +60,7 @@ object Evaluator {
     case MList(MSymbol("def!") :: rest) => define(ast, env, rest)
     case MList(MSymbol("let*") :: rest) => handleLet(ast, env, rest)
     case MList(MSymbol("do") :: rest) => handleDo(ast, env, rest)
+    //    case MList(MSymbol("if") :: rest) => handleIf(ast, env, rest)
     case list: MList => applyFunction(ast, env, list)
     case _ => Left(MalEvaluationError("Invalid list expression", ast))
   }
@@ -48,69 +91,19 @@ object Evaluator {
     } yield result
   }
 
-  private def handleLet(ast: MList, env: Environment, rest: List[MalExpr]) = {
-    rest match {
-      case (MList(definitions) :: expr :: Nil) =>
-        letEnv(definitions, env).flatMap { newEnv =>
-          evaluate(expr, newEnv)
-        }
-      case (MVector(definitions) :: expr :: Nil) =>
-        letEnv(definitions, env).flatMap { newEnv =>
-          evaluate(expr, newEnv)
-        }
-      case _ => Left(MalEvaluationError("Illegal 'let*' expression", ast))
-    }
-  }
-
-  private def letEnv(defs: List[MalExpr], outer: Environment): Either[MalError, Environment] = {
-    val newEnv = new Environment(Some(outer))
-
-    val definitions = liftError(defs.grouped(2).map {
-      case (key: MSymbol) :: value :: Nil => Right((key, value))
-      case elements => Left(MalEvaluationError(s"Key '${elements.head}' is not a symbol", MList(defs)))
-    }.toSeq)
-    definitions.flatMap { dfs =>
-      dfs.foldLeft(Right(newEnv): Either[MalError, Environment]) { case (eitherEnv, (key, value)) =>
-        for {
-          env <- eitherEnv
-          evaluated <- evaluate(value, env)
-        } yield {
-          newEnv.define(key, evaluated)
-          newEnv
-        }
-      }
-    }
-  }
-
-  def handleDo(ast: MList, env: Environment, rest: List[MalExpr]): Either[MalError, MalExpr] = {
-    val it = rest.iterator
-    var evaluated: Either[MalError, MalExpr] = null
-    while (it.hasNext) {
-      evaluated = evaluate(it.next(), env)
-      if(evaluated.isLeft) return evaluated
-    }
-    evaluated
-  }
-
-  private def liftError[E, V](s: Seq[Either[E, V]]): Either[E, List[V]] = {
-    s.foldRight(Right(Nil): Either[E, List[V]]) {
-      (either, acc) => for (xs <- acc; x <- either) yield x :: xs
-    }
-  }
-
-  private def liftError[E, K, V](m: Map[K, Either[E, V]]): Either[E, Map[K, V]] = {
-    m.foldRight(Right(Map()): Either[E, Map[K, V]]) {
-      case ((key, either), acc) => for (m <- acc; value <- either) yield m + (key -> value)
-    }
-  }
-
-  def evaluate_ast(ast: MalExpr, env: Environment): Either[MalError, MalExpr] = ast match {
+  private def evaluate_ast(ast: MalExpr, env: Environment): Either[MalError, MalExpr] = ast match {
     case sym: MSymbol => env.get(sym)
     case MList(elems) => elems match {
-      case _ => liftError(elems.map(evaluate(_, env))).map { items => MList(items) }
+      case _ => elems.traverseU(evaluate(_, env)).map { items => MList(items) }
     }
-    case MVector(elems) => liftError(elems.map(evaluate(_, env))).map { items => MVector(items) }
-    case MMap(elems) => liftError(elems.mapValues(evaluate(_, env))).map { m => MMap(m) }
+    case MVector(elems) => elems.traverseU(evaluate(_, env)).map { items => MVector(items) }
+    case MMap(elems) => elems.mapValues(evaluate(_, env)).sequenceU.map { m => MMap(m) }
     case _ => Right(ast)
   }
+
+  def evaluate(ast: MalExpr, env: Environment): Either[MalError, MalExpr] = ast match {
+    case list: MList => applyAst(list, env)
+    case _ => evaluate_ast(ast, env)
+  }
+
 }
