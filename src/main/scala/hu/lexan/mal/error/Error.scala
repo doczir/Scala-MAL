d70627bd8960636f6c8a35b4b1ed43e44ff2a9d3
Robert Doczi <doczi.r@gmail.com>
2017-01-29 02:33:42 +0100

    made errors throwables

--- src/main/scala/hu/lexan/mal/error/Error.scala
+++ src/main/scala/hu/lexan/mal/error/Error.scala
@@ -2,7 +2,7 @@ package hu.lexan.mal.error
 
 import hu.lexan.mal.ast.{MalExpr, MalParser}
 
-trait MalError
+trait MalError extends Throwable
 
 case class MalParseError(msg: String, source: MalParser.Input) extends MalError
 case class MalEvaluationError(msg: String, node: MalExpr) extends MalError
