626db8616072fda947c0807c46eebf70eddec644
Robert Doczi <doczi.r@gmail.com>
2017-01-29 17:51:48 +0100

    modified evaluation error to accept node as optional

--- src/main/scala/hu/lexan/mal/error/Error.scala
+++ src/main/scala/hu/lexan/mal/error/Error.scala
@@ -1,9 +1,10 @@
 package hu.lexan.mal.error
 
-import hu.lexan.mal.ast.{MalExpr, MalParser}
+import hu.lexan.mal.ast.{MNil, MalExpr, MalParser}
 
 trait MalError extends Throwable
 
 case class MalParseError(msg: String, source: MalParser.Input) extends MalError
-case class MalEvaluationError(msg: String, node: MalExpr) extends MalError
+
+case class MalEvaluationError(msg: String, node: MalExpr = MNil) extends MalError
 case class MalCompositeError(errors: List[MalError]) extends MalError
